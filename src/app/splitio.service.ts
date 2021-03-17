b7fd93f06ff8c3f48238a0b9c27036889df6d8a6
Emiliano Fant <emiliano.fant@split.io>
2021-03-17 16:50:58 -0300

    feat: changed the Splits service to show place holders, added some document and a couple of code improvements

--- src/app/splitio.service.ts
+++ src/app/splitio.service.ts
@@ -1,48 +1,65 @@
 import { Injectable } from '@angular/core';
-import { SplitFactory } from '@splitsoftware/splitio';
-import { fromEvent } from 'rxjs';
+import { SplitFactory } from '@splitsoftware/splitio-browserjs';
+import { fromEvent, Subscription } from 'rxjs';
 
 @Injectable()
 export class SplitioService {
-
+  /**
+   * The local reference to the Split SDK.
+   */
   splitio: SplitIO.ISDK;
+  /**
+   * The local reference to the Split SDK's Client.
+   */
   splitClient: SplitIO.IClient;
+  /**
+   * Flag to determine if SDK is ready or not.
+   */
   isReady = false;
+  /**
+   * The local reference to the list of Treatments.
+   */
   treatments: SplitIO.Treatments;
+  /**
+   * The list of Features/Split from which to get the corresponding
+   * treatments.
+   */
   features: string[] = [
-    'feature_1',
-    'feature_2',
-    'feature_3'
+    'feature-A',
+    'feature-B',
+    'feature-C'
   ];
-
-  constructor() { }
-
+  /**
+   * The local reference to the SDK's ready Observable.
+   */
+  subscription: Subscription;
+  /**
+   * This method initializes the SDK with the required Browser APIKEY
+   * and the 'key' according to the Traffic type set (ex.: an user id).
+   *
+   * @returns void
+   */
   initSdk(): void {
-    // Running the SDK in 'off-the-grid' Mode since authorizationKey : 'localhost'
-    // To bind a non 'off-the-grid' client, inject the real API Key
     this.splitio = SplitFactory({
       core: {
-        authorizationKey: 'localhost',
-        key: 'customer-key'
+        authorizationKey: '<YOUR_SDK_BROWSER_APIKEY>',
+        key: '<USER_ID>'
       },
-      // In non-localhost mode, this map is ignored.
-      features: {
-        feature_1: 'off',
-        feature_2: 'on',
-        feature_3: 'v2'
-      }
     });
-
     this.splitClient = this.splitio.client();
-
     // verify if sdk is initialized
     this.verifyReady();
   }
-
+  /**
+   * Function to check if the SDK is ready, subscribe to an Observable
+   * and set the isReady flag according to the result.
+   *
+   * @returns void
+   */
   private verifyReady(): void {
     const isReadyEvent = fromEvent(this.splitClient, this.splitClient.Event.SDK_READY);
 
-    const subscription = isReadyEvent.subscribe({
+    this.subscription = isReadyEvent.subscribe({
       next() {
         this.isReady = true;
         console.log('Sdk ready: ', this.isReady);
@@ -50,12 +67,24 @@ export class SplitioService {
       error(err) {
         console.log('Sdk error: ', err);
         this.isReady = false;
+        this.unsubscribeSDK();
       }
     });
   }
-
+  /**
+   * Function to get and set the treatments from the Split API.
+   *
+   * @returns void
+   */
   getTreatments(): void {
     this.treatments = this.splitClient.getTreatments(this.features);
   }
-
+  /**
+   * Function to unsubscribe the Observable from the SDK initialization.
+   *
+   * @returns void
+   */
+  unsubscribeSDK(): void {
+    this.subscription.unsubscribe();
+  }
 }
